3de44b8b432be1459aff9839682c5f9ca02518e2
dav24github <davidthbsossa@gmail.com>
2018-06-28 22:08:52 -0400

    primer commit

---
+++ ClienteELAPAS_CESSA/ClienteELAPAS_CESSA/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteELAPAS_CESSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServicioCESSA.WebService1SoapClient wsC = new ServicioCESSA.WebService1SoapClient();
+            ServicioELAPAS.WebService1SoapClient wsE = new ServicioELAPAS.WebService1SoapClient();
+
+            int id = int.Parse(textBox1.Text);
+
+            label2.Text = "Elapas -> " + wsE.getDeudaELAPAS(id);
+            label3.Text = "Cessa -> " + wsC.getDeudaCESSA(id);
+            label4.Text = "Total -> " + (wsE.getDeudaELAPAS(id) + wsC.getDeudaCESSA(id));
+        }
+    }
+}
---
+++ ServicioWebCESSA/ServicioWebCESSA/WebService1.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ServicioWebCESSA
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public Double getDeudaCESSA(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    return 100;
+                case 3:
+                    return 115;
+                case 4:
+                    return 50;
+                case 5:
+                    return 25;
+
+            }
+            return 0;
+        }
+    }
+}
